bbea3b43a079f8ceee8ba673b8be929e9c348dd2
MuhammadMaulanaEvandra <evannihbos20@gmil.com>
2026-08-31 15:33:41 +0700

    Pertemuan 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\mahasiswaController;
+use App\Http\Controllers\DashboardController;
+
+// Materi 2
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', function () {
+    return ('Halaman Profile');
+}) ->name('biodata');
+
+Route::get('/home', function () {
+    return '<a href="' . route('biodata') . '"> ke Profile</a>';
+}); 
+
+// Praktikum 2
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('dashboard');
+
+// Materi 3
+Route::get('/mahasiswa', function (){
+    return 'Hello, Muhammad Maulana Evandra. Selamat Datang!!!';
+});    
+
+
