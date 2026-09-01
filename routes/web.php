bd65634a8a3a07031cd9fabd54303a8faaaf9348
MuhammadMaulanaEvandra <evannihbos20@gmil.com>
2026-09-01 16:58:40 +0700

    Tugas Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,20 @@ Route::get('/profile', function () {
 
 Route::get('/home', function () {
     return '<a href="' . route('biodata') . '"> ke Profile</a>';
-}); 
+});
 
 // Praktikum 2
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth'])
     ->name('dashboard');
 
+Route::get('/about', function () {
+    return 'Selamat datang di Barokah Mart. Barokah Mart adalah toko yang menyediakan berbagai kebutuhan sehari-hari.';
+});
+
 // Materi 3
 Route::get('/mahasiswa', function (){
     return 'Hello, Muhammad Maulana Evandra. Selamat Datang!!!';
-});    
+});
 
 
