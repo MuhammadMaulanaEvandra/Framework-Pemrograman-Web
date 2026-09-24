538a0dd45841c1f62225502bef825b4c264d57fe
MuhammadMaulanaEvandra <evannihbos20@gmil.com>
2026-09-24 22:43:32 +0700

    Praktikum 6

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Product extends Model
 {
@@ -14,16 +15,22 @@ class Product extends Model
         'price',
         'stock',
     ];
-    
+
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
- 
+
     public function transactionDetails()
     {
         return $this->hasMany(TransactionDetail::class);
     }
 
+    protected function priceRupiah(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value, $attributes) => 'Rp ' . number_format($attributes['price'], 0, ',', '.')
+        );
+    }
 
 }
